773625d4fef0fd42883e136a1732b71d78cfcc0e
David Ribeiro <david.dmr@gmail.com>
2023-04-18 13:26:28 -0400

    Add temporary instances of PlayerRepository, PlayerUseCase and Api to PlayersViewModel | Make a request call to fetchPlayers

--- app/src/main/java/com/david/nbawatcher/viewmodels/PlayersViewModel.kt
+++ app/src/main/java/com/david/nbawatcher/viewmodels/PlayersViewModel.kt
@@ -1,21 +1,66 @@
 package com.david.nbawatcher.viewmodels
 
-import android.os.Handler
-import android.os.Looper
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.david.nbawatcher.data.mappers.PlayerMapper
+import com.david.nbawatcher.data.mappers.TeamMapper
+import com.david.nbawatcher.domain.models.Player
+import com.david.nbawatcher.data.network.Api
+import com.david.nbawatcher.data.repositories.PlayerDataRepository
+import com.david.nbawatcher.ui.fragments.PlayersFragment
+import com.david.nbawatcher.domain.usecases.PlayerUseCaseImpl
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-class PlayersViewModel : ViewModel() {
+class PlayersViewModel(
+//    private val playerUseCase: PlayerUserCase
+    ) : ViewModel() {
 
-    private val _playersList = MutableLiveData<String>()
+    private val _playersList = MutableLiveData<List<Player>>()
 
-    val playerList: LiveData<String>
+    val playerList: LiveData<List<Player>>
         get() = _playersList
 
-    fun updateTitle() {
-        Handler(Looper.myLooper()!!).postDelayed({
-            _playersList.value = "Text changed"
-        }, 3000L)
+    var logging: HttpLoggingInterceptor? = null
+
+    init {
+        logging = HttpLoggingInterceptor()
+        logging!!.level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val httpClient = OkHttpClient.Builder()
+        .addInterceptor(logging!!)
+        .build()
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .client(httpClient)
+        .baseUrl("https://balldontlie.io/api/v1/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    private val api = retrofit.create(Api::class.java)
+
+    private val teamMapper = TeamMapper()
+    private val mapper = PlayerMapper(teamMapper)
+    private val repository = PlayerDataRepository(api, mapper)
+
+    private val playerUseCase = PlayerUseCaseImpl(repository)
+    fun fetchAllPlayers() {
+
+        viewModelScope.launch(exceptionHandler()) {
+            val result = playerUseCase.fetchPlayers()
+            _playersList.value = result
+        }
+    }
+
+    private fun exceptionHandler() = CoroutineExceptionHandler { coroutineContext, throwable ->
+        Log.d(PlayersFragment.TAG, "onError: $throwable")
     }
 }
