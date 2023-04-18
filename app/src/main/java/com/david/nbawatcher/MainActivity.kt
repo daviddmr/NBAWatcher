443d6a0bda8ed8be6975749a5698cdb270be19bc
David Ribeiro <david.dmr@gmail.com>
2023-04-17 21:12:35 -0400

    Begin of the journey!

---
+++ app/src/main/java/com/david/nbawatcher/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.david.nbawatcher
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.david.nbawatcher.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
